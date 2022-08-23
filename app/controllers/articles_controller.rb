cf7fd8f442658742cca9c70a6d75a74006790d77
thomis <capuchothomas@hotmail.com>
2022-08-23 20:06:04 -0300

    add show articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,7 @@ class ArticlesController < ApplicationController
     @articles= Article.all #listando os articles
     #@ deixa ela publica para a view
   end
+  def show
+    @article = Article.find(params[:id]) #cada article tem um id, passando como parametro no browser ele resgata
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'articles#index' #definindo tela inicial
-  get '/articles', to: 'articles#index' #indo pro controler articles na action index
+  #get '/articles', to: 'articles#index' #indo pro controler articles na action index
+  #get '/articles/:id', to: 'articles#show'
+
+  resources :articles
 end
