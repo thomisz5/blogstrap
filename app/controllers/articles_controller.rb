9095e0fd09d6e5b9bf492619b0bf07f562b3b9e3
thomas <capuchothomas@hotmail.com>
2022-09-02 16:27:19 -0300

    add article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id]) #cada article tem um id, passando como parametro no browser ele resgata
   end
+
+  def new
+    @article=Article.new
+  end
+
+  def create
+    @article=Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title,:body)
+    #permitindo somente o preenchimento dos campos com title e body
+  end
 end
