edcccf021addba7e30e953f2f90873f4df447549
thomas <capuchothomas@hotmail.com>
2022-09-09 14:50:31 -0300

    add article update/form partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,20 @@
 class ArticlesController < ApplicationController
+  #criação das actions
   def index
-    @articles= Article.all #listando os articles
+    @articles = Article.all #listando os articles
     #@ deixa ela publica para a view
   end
+
   def show
     @article = Article.find(params[:id]) #cada article tem um id, passando como parametro no browser ele resgata
   end
 
   def new
-    @article=Article.new
+    @article = Article.new
   end
 
   def create
-    @article=Article.new(article_params)
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -21,9 +23,24 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
   private
+
   def article_params
-    params.require(:article).permit(:title,:body)
+    params.require(:article).permit(:title, :body)
     #permitindo somente o preenchimento dos campos com title e body
   end
 end
